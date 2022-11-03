30c3f38ad5bc50dd2e9e2e2cbf697db7e0a84541
Mickael Bretin <mickael.bretin@gmail.com>
2022-11-03 11:17:36 +0100

    added branch feature2

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ceci est ma nouvelle branch feature 2
         </a>
       </header>
     </div>
