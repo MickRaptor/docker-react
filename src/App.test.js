00e93eb273da08f1e48638282b5772cc356ddbfc
Mickael Bretin <mickael.bretin@gmail.com>
2022-11-03 15:41:48 +0100

    feature4

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react V2/i);
+  const linkElement = screen.getByText(/Coucou Ceci Fonctionne 2/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react V2/i);
+  const linkElement = screen.getByText(/Coucou Ceci Fonctionne 2/i);
   expect(linkElement).toBeInTheDocument();
 });
